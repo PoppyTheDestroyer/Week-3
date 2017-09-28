21678640282486374f9ec225dda23bab9886d3d8
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-09-28 11:10:45 -0500

    dunno

--- variables.js
+++ variables.js
@@ -1,6 +1,14 @@
-var fred = 32;
-var i = "e"
-fred = "taco"
-fred = i
+connst name = "Jason"
+const age = 36
 
-console.log(fred)
+if (name = "Jason") {
+    console.log("Yes, your name is Jason");
+}
+
+if (age >= 30) {
+    console.log("You are old.");
+} else if (age >= 36 && age <40) {
+    console.log("Well at least you're not 40.")
+}
+else console.log("Sorry, dude.")
+
